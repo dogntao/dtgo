311cc93180771e3b9bd0d3628a9edd3b91e5f975
dongt <dongt@dongtaodeMac-mini.local>
2019-02-19 18:53:44 +0800

    init

--- controller.go
+++ controller.go
@@ -8,17 +8,28 @@ import (
 )
 
 type MainControler struct {
+	// 对应controller和action
 	Con, Act string
-	Params   map[string]string
-	ConMap   map[string]interface{}
+	// 相应信息
+	Rep http.ResponseWriter
+	// 参数
+	Params map[string]string
+	// 路由对应关系
+	ConMap map[string]interface{}
+	// 公共页面模板
+	Layouts string
+	// 页面传递参数
+
 }
 
+// 路由
 func (mainc *MainControler) Router(w http.ResponseWriter, r *http.Request) {
 	url := r.URL
 	path := url.Path
 	if path != "/favicon.ico" {
 		// 处理controllre和action
 		pathArr := strings.Split(path, "/")
+		// 首字母大写
 		mainc.Con = strings.Title(pathArr[1])
 		mainc.Act = strings.Title(pathArr[2])
 		// 处理参数
@@ -27,8 +38,7 @@ func (mainc *MainControler) Router(w http.ResponseWriter, r *http.Request) {
 				mainc.Params[k] = v[0]
 			}
 		}
-		fmt.Println(mainc.Params)
-
+		mainc.Rep = w
 		// 通过反射调用方法
 		conv, exist := mainc.ConMap[mainc.Con]
 		if exist == true {
@@ -44,4 +54,10 @@ func (mainc *MainControler) Router(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	// fmt.Println(url.RawQuery)
+}
+
+// 解析页面
+func (mainc *MainControler) Display(page string) {
+	// tem, _ := template.ParseFiles(page, mainc.Layouts)
+	// tem.ExecuteTemplate(mainc.Rep, page)
 }
