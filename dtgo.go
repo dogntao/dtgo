7339b931fad148ff6474ad2547d8909897eaa6a2
dongt <dongtao@haowj.com>
2020-06-10 23:44:59 +0800

    增加static

--- dtgo.go
+++ dtgo.go
@@ -9,18 +9,20 @@ type Host struct {
 	CSS     string // 静态css路径(/css/)
 	Image   string // 静态图片路径(/image/)
 	Uploads string // 静态上传路径(/uploads/)
+	Static  string // 静态上传路径(/static/)
 	Port    string // 端口
 	Router  *RouterStruct
 }
 
 // NewHost 初始化Host
-func NewHost(public, js, css, image, uploads, port string) *Host {
+func NewHost(public, js, css, image, uploads, static, port string) *Host {
 	return &Host{
 		Public:  public,
 		Js:      js,
 		CSS:     css,
 		Image:   image,
 		Uploads: uploads,
+		Static:  static,
 		Port:    port,
 		Router:  NewRouterStruct(),
 	}
@@ -36,6 +38,8 @@ func (host *Host) Run() {
 	http.Handle(host.Image, http.FileServer(http.Dir(host.Public)))
 	// 上传文件路径
 	http.Handle(host.Uploads, http.FileServer(http.Dir(host.Public)))
+	// 上传文件路径
+	http.Handle(host.Static, http.FileServer(http.Dir(host.Public)))
 	http.HandleFunc("/", host.Router.Router)
 	http.ListenAndServe(host.Port, nil)
 }
